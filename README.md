f9a01656a1c874ac19d1428a1923c59208d1fce3
MetaMsa <mserhataslan@hotmail.com>
2024-05-24 00:03:58 +0300

    main

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CodLe
-Programming languages quiz like wordle,travle,loldle.
+Programlama dilleri ile ilgili travle, wordle, loldle gibi bir bilgi oyunu.
+
+## https://mehmetserhataslan.tech/game
+
+### Oynanış
+
+3 kriterde günün programlama dilini tahmin edin.
+
+1- Çıkış Yılı <br>
+2- Derlenen mi? Yorumlanan mı? <br>
+3- C türevi mi? <br>
+
+24 saatte bir dil değişir.
+
+Şimdilik sadece 10 bilinen dil var. Geliştirilecektir.
---
+++ game.js
@@ -0,0 +1,120 @@
+var namee, year, comp, c; 
+
+var time=new Date("2024-05-23");
+var today=new Date();
+var timestamp=Math.floor((today-time) / (1000*60*60*24));
+
+var data=fetch("data/lang.json")
+    .then(response => response.json())
+    .then(data => {var index = timestamp % data.length;
+                   namee = data[index].name;
+                   year = data[index].year;
+                   comp = data[index].compiled;
+                   c = data[index].c;
+    });
+
+var count=0;
+var match = () => {
+    var a=document.getElementById("game").value.toLowerCase().trim();
+
+    var data=fetch("/data/lang.json")
+    .then(response => response.json())
+    .then(data => {
+        for(let i=0;i<data.length;i++) 
+            {
+            if (data[i].name == a) {
+    
+                if (data[i].name == namee) 
+                {
+                    document.getElementById("gamebox1").style.backgroundColor = "yellow";
+                    document.getElementById("gamebox2").style.backgroundColor = "yellow";
+                    document.getElementById("gamebox3").style.backgroundColor = "yellow";
+
+                    document.getElementById("game1-text").innerHTML = year;
+                    document.getElementById("yeartext").innerHTML = "";
+                    document.getElementById("game2-text").innerHTML = comp;
+                    document.getElementById("game3-text").innerHTML = c;
+
+                    alert("Tebrikler! Doğru dil adını buldunuz!");
+
+                    count++;
+                }
+
+                else{
+                        if(data[i].year > year)
+                        {
+                            document.getElementById("yeartext").innerHTML = "daha eski bir dil";
+
+                            document.getElementById("gamebox1").style.backgroundColor = "red";
+
+                            count++;
+                        }
+
+                        if(data[i].year == year)
+                        {
+                            document.getElementById("gamebox1").style.backgroundColor = "yellow";
+    
+                            count++;
+                        }
+
+                        if(data[i].year < year)
+                        {
+                            document.getElementById("yeartext").innerHTML = "daha yeni bir dil";
+
+                            document.getElementById("gamebox1").style.backgroundColor = "red";
+
+                            count++;
+                        }
+            
+                        if(data[i].compiled == comp) 
+                        {
+                            document.getElementById("gamebox2").style.backgroundColor = "yellow";
+
+                            count++;
+                        }
+
+                        if(data[i].compiled != comp) 
+                            {
+                                document.getElementById("gamebox2").style.backgroundColor = "red";
+
+                                count++;
+                            }
+            
+                        if(data[i].c == c) 
+                        {
+                            document.getElementById("gamebox3").style.backgroundColor = "yellow";
+
+                            count++;
+                        }
+
+                        if(data[i].c != c) 
+                            {
+                                document.getElementById("gamebox3").style.backgroundColor = "red";
+
+                                count++;
+                            }
+
+                        document.getElementById("game1-text").innerHTML = data[i].year;
+                        document.getElementById("game2-text").innerHTML = data[i].compiled;
+                        document.getElementById("game3-text").innerHTML = data[i].c;
+                }
+            }
+        }
+
+        if(count==0)
+        {
+            alert("Hatalı dil adı girdiniz! ");
+
+            document.getElementById("yeartext").innerHTML = "";
+
+            document.getElementById("game1-text").innerHTML = "";
+            document.getElementById("game2-text").innerHTML = "";
+            document.getElementById("game3-text").innerHTML = "";
+
+            document.getElementById("gamebox1").style.backgroundColor = "red";
+            document.getElementById("gamebox2").style.backgroundColor = "red";
+            document.getElementById("gamebox3").style.backgroundColor = "red";
+        }   
+        count=0;
+    });
+}
